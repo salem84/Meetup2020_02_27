8024dcb2c09416a03b3427ed7dd4882346a0e539
salem84 <salem84@users.noreply.github.com>
2020-02-12 00:14:01 +0100

    sql database

--- src/Food/Food.API/Startup.cs
+++ src/Food/Food.API/Startup.cs
@@ -32,10 +32,20 @@ namespace Food.API
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services, IWebHostEnvironment env)
         {
             services.AddOptions();
-            services.AddDbContext<FoodDbContext>(opt => opt.UseInMemoryDatabase("FoodDatabase"));
+            services.AddDbContext<FoodDbContext>(opt =>
+            {
+                if (env.IsDevelopment())
+                {
+                    opt.UseInMemoryDatabase("FoodDatabase");
+                }
+                else
+                {
+                    opt.UseSqlServer(Configuration.GetConnectionString("FoodDatabaseConnString"));
+                }
+            });
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAllOrigins",
