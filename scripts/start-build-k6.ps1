875c758bfd8ace0185295ac64ffc0c08cfddb679
salem84 <salem84@users.noreply.github.com>
2020-02-16 23:51:59 +0100

    script

---
+++ scripts/start-build-k6.ps1
@@ -0,0 +1,8 @@
+﻿Write-Host "Leggo build nella cartella K6"
+$pipelines = az pipelines list --query "[?path=='\K6'].{Nome: name, Id: id}" | ConvertFrom-Json
+$pipelines | 
+    #Select-Object -first 1 | 
+    foreach {
+        Write-Host "Lancio Build [$($_.Id)] $($_.Nome)"
+        az pipelines run --id $_.Id
+    }
